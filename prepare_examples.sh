ec2e982fe6a66fbf763ba31f44b1a7c7f8027ed2
nageshpc <nageshpc@users.noreply.github.com>
2017-02-22 11:38:52 +1100

    Update prepare_examples.sh

--- prepare_examples.sh
+++ prepare_examples.sh
@@ -18,5 +18,5 @@ cat train_examples.txt dev_examples.txt > train_and_dev_examples.txt
 
 find data/BioNLP-ST-2016_SeeDev-binary_test/   -iname "*.txt" | while read fname ; do 
 	documentid=${fname/.txt/};  
-	python preprocess.py $documentid data/  > test_examples.txt
+	python preprocess.py $documentid data/  >> test_examples.txt
 done
